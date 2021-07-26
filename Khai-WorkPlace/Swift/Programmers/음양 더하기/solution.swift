95e0033415a15769299b4057862c71606f55b22b
IceAmerican0 <starsjun@naver.com>
2021-07-26 13:39:06 +0900

    2021-07-26 박성준
    [2021-07-26] 박성준 완료!

---
+++ Khai-WorkPlace/Swift/Programmers/음양 더하기/solution.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+    var sum=0
+    
+    for i in 0..<absolutes.count{
+        sum += signs[i] == true ? absolutes[i] : -absolutes[i]
+    }
+    
+    return sum
+}
