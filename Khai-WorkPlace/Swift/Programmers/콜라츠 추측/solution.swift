21fa29cb7b94fcd453704c93df915ac2cb41eecc
IceAmerican0 <starsjun@naver.com>
2021-08-12 16:32:54 +0900

    2021-08-12 박성준
    [2021-08-12] 박성준 완료!

---
+++ Khai-WorkPlace/Swift/Programmers/콜라츠 추측/solution.swift
@@ -0,0 +1,15 @@
+func solution(_ num:Int) -> Int {
+    var count = 0
+    var number = num
+    
+    while number != 1{
+        if(number%2==0){
+            number = number/2
+        }else{
+            number = number*3+1
+        }
+        count += 1
+    }
+    
+    return count >= 500 ? -1 : count
+}
