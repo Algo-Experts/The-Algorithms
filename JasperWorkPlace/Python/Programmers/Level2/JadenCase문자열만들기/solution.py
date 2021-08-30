b4bdcd8ccc6796dd6780526dcc9d560e8e7f5858
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-08-30 23:52:40 +0900

    2021-08-30 오영준

---
+++ JasperWorkPlace/Python/Programmers/Level2/JadenCase문자열만들기/solution.py
@@ -0,0 +1,20 @@
+# JadenCase란 모든 단어의 첫 문자가 대문자이고,
+# 그 외의 알파벳은 소문자인 문자열입니다.
+# 문자열 s가 주어졌을 때,
+#  s를 JadenCase로 바꾼 문자열을 리턴하는 함수,
+#  solution을 완성해주세요.
+
+
+def solution(s):
+    answer = ''
+    for idx, i in enumerate(s):
+        if (idx == 0):
+            answer = i.upper()
+        elif s[idx-1] == ' ':
+            answer = ''.join([answer, i.upper()])
+        else:
+            answer = ''.join([answer, i.lower()])
+    return answer
+
+
+print(solution("3people unFollowed me"))
