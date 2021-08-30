aa0a5907d39cf3721e99314f09169376cbb58050
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-08-31 00:03:58 +0900

    2021-08-30 오영준

--- JasperWorkPlace/Python/Programmers/Level2/N개의최소공배수/solution.py
+++ JasperWorkPlace/Python/Programmers/Level2/N개의최소공배수/solution.py
@@ -1,10 +1,13 @@
+# 각각의 최소공배수를 구해서 나타내면 되는 것으로 보인다.
+# For example, if the array is [ 2 , 6,8, 14] 이면, 2 와 6의 최소공배수 를 a 로 가정하면,  a 와 8의 최
+# 소 공배수를 다시 b 로 그리고 b 와 14 의 최소공배수가 바로 정답이 나온다.,
+
+
+# 최대공약수 뽑는 알고
 def gcd(a, b):
-    mod = a % b
-    while mod > 0:
-        a = b
-        b = mod
-        mod = a % b
-    return b
+    while(b):
+        a, b = b, a % b
+    return a
 
 
 def solution(arr):
@@ -18,3 +21,6 @@ def solution(arr):
 
     answer = arr[-1]
     return answer
+
+
+print(gcd(3, 4))
